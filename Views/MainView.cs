65539c77a609c6b9d9e9e5b550ec0939b52170f0
Zdravko Danev <zdanev@gmail.com>
2025-07-14 17:41:30 -0700

    added feed items and content views

--- Views/MainView.cs
+++ Views/MainView.cs
@@ -1,13 +1,17 @@
+namespace TuiNews.Views;
+
 using Terminal.Gui;
 using Terminal.Gui.Graphs;
 using TuiNews.Services;
-
-namespace TuiNews.Views;
 
 public class MainView : Window
 {
   private readonly FeedsService feedsService;
   private readonly ListView feedsListView;
+  private readonly ListView feedItemsListView;
+  private readonly Label titleLabel;
+  private readonly Label urlLabel;
+  private readonly TextView contentTextView;
 
   public MainView(FeedsService feedsService) : base("TUI News")
   {
@@ -34,9 +38,54 @@ public class MainView : Window
       Y = 0,
       Height = Dim.Fill()
     };
-    Add(verticalLine);
+    // Add(verticalLine);
 
+    feedItemsListView = new ListView()
+    {
+      X = Pos.Right(verticalLine),
+      Y = 0,
+      Width = Dim.Fill(),
+      Height = 10
+    };
+    Add(feedItemsListView);
 
+    var horizontalLine = new LineView(Orientation.Horizontal)
+    {
+      X = Pos.Right(feedsListView),
+      Y = Pos.Bottom(feedItemsListView),
+      Width = Dim.Fill()
+    };
+    Add(horizontalLine);
+    Add(verticalLine); // !!!
+
+    titleLabel = new Label("Title:")
+    {
+      X = Pos.Right(verticalLine),
+      Y = Pos.Bottom(horizontalLine),
+      Width = Dim.Fill(),
+      Height = 1
+    };
+    Add(titleLabel);
+
+    urlLabel = new Label("URL:")
+    {
+      X = Pos.Right(verticalLine),
+      Y = Pos.Bottom(titleLabel),
+      Width = Dim.Fill(),
+      Height = 1
+    };
+    Add(urlLabel);
+
+    contentTextView = new TextView()
+    {
+      X = Pos.Right(verticalLine),
+      Y = Pos.Bottom(urlLabel),
+      Width = Dim.Fill(),
+      Height = Dim.Fill(),
+      ReadOnly = true,
+      WordWrap = true
+    };
+    Add(contentTextView);
 
     var statusBar = new StatusBar([
       new StatusItem(Key.CtrlMask | Key.R, "~^R~ Refresh Feed", null),
